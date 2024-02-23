99589e48483fdec551d10170bff00fe5753e503d
zeneke <zeneke@vindrogames.com>
2024-02-23 23:54:58 +0000

    testing first zip backup program

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.IO.Compression;
+
+namespace McBackup;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your name?");
+        var name = Console.ReadLine();
+        var currentDate = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}");
+        Console.WriteLine($"{Environment.NewLine}Press any key to exit.");
+        //Console.ReadKey(true);
+        CompressTest();
+    }
+
+    static void CompressTest()
+    {
+        string startPath = @"output/start";
+        string zipPath = @"output/result.zip";
+        string extractPath = @"output/extract";
+
+        if (File.Exists(zipPath))
+        {
+            File.Delete(zipPath);
+        }
+
+        try
+        {
+            var files = Directory.EnumerateFiles(extractPath);
+
+            foreach (string currentFile in files)
+            {
+                File.Delete(currentFile);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        ZipFile.CreateFromDirectory(startPath, zipPath);
+        ZipFile.ExtractToDirectory(zipPath, extractPath);
+    }
+}
